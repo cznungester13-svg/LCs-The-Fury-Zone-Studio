05496d2a2d37f5c0f97c2cc2577a48a27485efa9
cznungester13-svg <cznungester13@gmail.com>
2026-07-07 15:08:02 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -202,7 +202,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://marketplace-fury.preview.emergentagent.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
