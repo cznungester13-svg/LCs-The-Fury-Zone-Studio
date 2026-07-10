836bae965ea906d81823082226add08b884bde82
cznungester13-svg <cznungester13@gmail.com>
2026-07-09 21:05:24 -0500

    Create Community.jsx

---
+++ frontend/src/pages/Community.jsx
@@ -0,0 +1,11 @@
+export default function Community() {
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-black uppercase tracking-tighter">Chatting with my girls!</h1>
+      <div className="mt-6 border-2 border-black p-4">
+        {/* Chat content goes here */}
+        <p>Loading the conversation...</p>
+      </div>
+    </div>
+  );
+}
