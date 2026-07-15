d6d77a24c4a2b2f6e297b3e9a93bf30a43d44a31
cznungester13-svg <cznungester13@gmail.com>
2026-07-14 23:19:42 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,17 @@
+import os
+from datetime import datetime, timezone
+from motor.motor_asyncio import AsyncIOMotorClient
 
+mongo_url = os.environ.get("MONGO_URL", "")
+
+client = AsyncIOMotorClient(mongo_url)
+
+db_name = os.environ.get("DB_NAME", "fury_zone")
+db = client[db_name]
+
+
+def now_iso() -> str:
+    return datetime.now(timezone.utc).isoformat()
+
+
+NO_ID = {"_id": 0}
