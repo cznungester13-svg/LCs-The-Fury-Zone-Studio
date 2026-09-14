ecb17c5e005ec23f2728645eb8bd1b49dcc513e5
cznungester13-svg <cznungester13@gmail.com>
2026-09-14 14:42:21 +0000

    Fix api/index.py imports and root requirements

--- api/index.py
+++ api/index.py
@@ -1,1 +1,4 @@
-{"rewrites": [{"source": "/api/(.*)", "destination": "/api/index.py"}]} -e import sys\nimport os\nsys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))\nfrom server import app
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
+from server import app
